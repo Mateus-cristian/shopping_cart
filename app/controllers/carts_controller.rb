9942ef6c2077a8438172289b40bccb96021b2907
mateus cristian <mateus_cristian@outlook.com>
2026-01-30 07:54:27 -0300

    refactor: streamline cart creation and add_item methods for clarity

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 class CartsController < ApplicationController
-  before_action :current_cart
+  before_action :current_cart, only: %i[show add_item]
 
   def show
-    render json: current_cart, include: :cart_items
+    render json: current_cart
   end
 
   def create
@@ -19,12 +19,26 @@ class CartsController < ApplicationController
     return render json: { error: 'Produto não encontrado' }, status: :not_found unless product
 
     begin
-      cart = ::Carts::AddItemService.new(cart: @current_cart, product:, quantity:).call
+      cart = build_cart_with_product(product, quantity)
+      session[:cart_id] = cart.id
       render json: cart, status: :ok
-    rescue ArgumentError => e
+    rescue ActiveRecord::RecordInvalid => e
       render json: { error: e.message }, status: :unprocessable_entity
+    end
+  end
+
+  def add_item
+    product_id = params[:product_id]
+    quantity = params[:quantity].to_i
+    product = Product.find_by(id: product_id)
+
+    begin
+      cart = Carts::AddItemService.new(cart: current_cart, product:, quantity:).call
+      render json: cart, status: :ok
     rescue ActiveRecord::RecordNotFound => e
       render json: { error: e.message }, status: :not_found
+    rescue ArgumentError, ActiveRecord::RecordInvalid => e
+      render json: { error: e.message }, status: :unprocessable_entity
     end
   end
 
@@ -41,5 +55,11 @@ class CartsController < ApplicationController
     cart = Cart.create!(total_price: 0)
     session[:cart_id] = cart.id
     @current_cart = cart
+  end
+
+  def build_cart_with_product(product, quantity)
+    cart = Cart.create!(total_price: 0)
+    cart.cart_items.create!(product:, quantity:, unit_price: product.price)
+    cart
   end
 end
